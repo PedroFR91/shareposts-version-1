5fbadcdf1eccca9dcb715ca9a389b03ad00a42c4
DevPed <pedromfr91@gmail.com>
2021-06-30 21:22:38 +0200

    Post Version 1

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+//DB params
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','123456');
+define('DB_NAME','shareposts');
+//App Root
+define('APPROOT',dirname(dirname(__FILE__)));
+// URL ROOT
+define('URLROOT','http://localhost/shareposts');
+//Site Name
+define('SITENAME','SharePost');
+//App Version
+define('APPVERSION','1.0.0');
